ebf68207e5e695853dc8e9716071a5aa51c1da64
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-06-30 10:43:57 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,21 @@ $(function(){
         $('.burger__burger,.header__menu').toggleClass('active');
         $('body').toggleClass('lock');
     });
+    $("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Сообщение отправлено!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
 
 
   
