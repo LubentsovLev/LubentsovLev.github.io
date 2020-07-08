7410a6852c78522e8065407ec489a0f17ac316db
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-07-08 15:16:07 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -18,12 +18,16 @@ $(function(){
 		});
 		return false;
 	});
-	$(".header__menu,.order__container").on("click",".a-1", function (event) {
+	$(".header__menu,.order__container,.footer").on("click",".a-1", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1000);
     });
+
+	$(".portfolio__btn").click(function() {
+		$('.portfolio__item').show();
+	  });
 
 
   
