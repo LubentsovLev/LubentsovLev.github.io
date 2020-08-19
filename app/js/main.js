44c95ef095ad66a0590f555a92694381e45e4021
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-08-19 18:46:50 +0300

    Add files via upload

--- app/js/main.js
+++ app/js/main.js
@@ -1,36 +1,38 @@
-$(function(){
-    $('.burger__burger').click(function(event){
-        $('.burger__burger,.header__menu').toggleClass('active');
-        $('body').toggleClass('lock');
+$(function () {
+  $(".burger__burger").click(function (event) {
+    $(".burger__burger,.header__menu").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
+  $("form").submit(function () {
+    //Change
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php", //Change
+      data: th.serialize(),
+    }).done(function () {
+      alert("Сообщение отправлено!");
+      setTimeout(function () {
+        // Done Functions
+        th.trigger("reset");
+      }, 1000);
     });
-    $("form").submit(function() { //Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", //Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Сообщение отправлено!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
-	$(".header__menu,.order__container,.footer").on("click",".a-1", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1000);
-    });
+    return false;
+  });
+  $(".header__menu,.order__container,.footer,.head__buttons").on(
+    "click",
+    ".a-1",
+    function (event) {
+      event.preventDefault();
+      var id = $(this).attr("href"),
+        top = $(id).offset().top;
+      $("body,html").animate({ scrollTop: top }, 1000);
+    }
+  );
 
-	$(".portfolio__btn").click(function() {
-		$('.portfolio__item').show();
-		$('.portfolio__btn').addClass('hide');
-		$('.portfolio__items').addClass('hide');
-	  });
-
-
-  
+  $(".portfolio__btn").click(function () {
+    $(".portfolio__item").show();
+    $(".portfolio__btn").addClass("hide");
+    $(".portfolio__items").addClass("hide");
+  });
 });
