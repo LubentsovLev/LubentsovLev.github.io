2e09c3e41bb91b6c4d7fb4bece9498fcd2e7c261
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-09-18 23:15:30 +0300

    Add files via upload

--- all/ircut/js/main.js
+++ all/ircut/js/main.js
@@ -1,6 +1,9 @@
-$(function(){
-
-
-
-  
+$(function () {
+  $(".growth__small-slider").slick({
+    infinite: true,
+    fade: false,
+    prevArrow: false,
+    nextArrow: false,
+    dots: true,
+  });
 });
