c424a152c9de27560984d91fb46390b723f23ff5
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-09-19 12:41:15 +0300

    Add files via upload

--- all/ircut/js/main.js
+++ all/ircut/js/main.js
@@ -6,4 +6,36 @@ $(function () {
     nextArrow: false,
     dots: true,
   });
+  let pop__1 = document.querySelector('.pop__1')
+  let pop__2 = document.querySelector('.pop__2')
+  let pop__3 = document.querySelector('.pop__3')
+  let pop__4 = document.querySelector('.pop__4')
+  let form = document.querySelector(".form__popup");
+  let form__inn = document.querySelector(".form__popup-inner");
+  let x = document.querySelector(".x");
+
+  const openPop = {
+    open: function () {
+      form.classList.add("block");
+      form__inn.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+      form__inn.classList.add('av')
+    },
+  };
+  x.addEventListener('click',() => {
+    form.classList.remove("block");
+    form__inn.style.backgroundColor = "transparent";
+      form__inn.classList.remove('av')
+  })
+  pop__1.addEventListener("click", () => {
+    openPop.open()
+  });
+  pop__2.addEventListener("click", () => {
+    openPop.open()
+  });
+  pop__3.addEventListener("click", () => {
+    openPop.open()
+  });
+  pop__4.addEventListener("click", () => {
+    openPop.open()
+  });
 });
