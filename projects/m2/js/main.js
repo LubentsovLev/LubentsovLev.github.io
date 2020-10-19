eeb3d11d1ab39c623453af3eb71d4cd6659e0668
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-10-20 00:52:31 +0300

    Add files via upload

---
+++ projects/m2/js/main.js
@@ -0,0 +1,24 @@
+$(function(){
+
+    $('.about__right').slick({
+        infinite: true,
+        slidesToShow: 1,
+        dots:true,
+        slidesToScroll: 1
+      });
+      $('.port__slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        dots:true,
+        slidesToScroll: 1
+      });
+      $('.rev__items').slick({
+        infinite: true,
+        slidesToShow: 1,
+        dots:true,
+        slidesToScroll: 1
+      });
+      
+
+      
+});
