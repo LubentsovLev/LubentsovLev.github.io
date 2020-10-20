9587f03193d87fb8478fc3716980b5e0c8bf8157
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-10-21 00:26:34 +0300

    Add files via upload

--- projects/m2/js/main.js
+++ projects/m2/js/main.js
@@ -1,24 +1,38 @@
-$(function(){
+$(function () {
+  $(".about__right").slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+  });
+  $(".port__slider").slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+  });
+  $(".rev__items").slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+  });
+  $(".privilege__items-mob").slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+  });
+  $(".clients__items-mob").slick({
+    infinite: true,
+    slidesToShow: 1,
+    dots: true,
+    slidesToScroll: 1,
+  });
 
-    $('.about__right').slick({
-        infinite: true,
-        slidesToShow: 1,
-        dots:true,
-        slidesToScroll: 1
-      });
-      $('.port__slider').slick({
-        infinite: true,
-        slidesToShow: 1,
-        dots:true,
-        slidesToScroll: 1
-      });
-      $('.rev__items').slick({
-        infinite: true,
-        slidesToShow: 1,
-        dots:true,
-        slidesToScroll: 1
-      });
-      
 
-      
+  $(".burger__burger").click(function (event) {
+    $(".burger__burger,.header__left-bottom").toggleClass("active");
+    $("body").toggleClass("lock");
+  });
 });
