7f8638033ca3902e38ee07c0ef98c3cc24c3a5e7
LubentsovLev <67600961+LubentsovLev@users.noreply.github.com>
2020-10-28 17:32:06 +0300

    Add files via upload

--- projects/security/js/main.js
+++ projects/security/js/main.js
@@ -3,18 +3,21 @@ let pop_order_1 = document.querySelector(".pop_order_1");
 let pop_order_2 = document.querySelector(".pop_order_2");
 let pop_order_3 = document.querySelector(".pop_order_3");
 let present__main_btn = document.querySelector(".present__main-btn");
+let which__bottom = document.querySelector(".which__bottom span");
 
 let popup_1 = document.querySelector(".popup_1");
 let popup_2 = document.querySelector(".popup_2");
 let popup_3 = document.querySelector(".popup_3");
 let popup_4 = document.querySelector(".popup_4");
 let popup_5 = document.querySelector(".popup_5");
+let popup_6 = document.querySelector(".popup_6");
 
 let x_1 = document.querySelector(".x_1");
 let x_2 = document.querySelector(".x_2");
 let x_3 = document.querySelector(".x_3");
 let x_4 = document.querySelector(".x_4");
 let x_5 = document.querySelector(".x_5");
+let x_6 = document.querySelector(".x_6");
 
 let popup = document.querySelector(".popup ");
 
@@ -43,12 +46,14 @@ openPop(pop_order_2, popup_3);
 openPop(pop_order_3, popup_4);
 openPop(footer__call, popup_1);
 openPop(present__main_btn, popup_5);
+openPop(which__bottom, popup_6);
 
 closePop(x_1, popup_1);
 closePop(x_2, popup_2);
 closePop(x_3, popup_3);
 closePop(x_4, popup_4);
 closePop(x_5, popup_5);
+closePop(x_6, popup_6);
 
 $(function () {
   $("form").submit(function () {
@@ -105,22 +110,36 @@ let quiz_i_3 = document.querySelector(".quiz_i_3");
 let quiz_i_4 = document.querySelector(".quiz_i_4");
 let quiz_i_5 = document.querySelector(".quiz_i_5");
 
-let quiz_i_1__in = document.querySelector(".quiz_i_1 input");
-let quiz_i_2__in = document.querySelector(".quiz_i_2 select");
-let quiz_i_3__in = document.querySelector(".quiz_i_3 input");
+let quiz_i_1__in = document.querySelector(".quiz_i_1 select");
+let quiz_i_2__in = document.querySelector(".quiz_i_2 input");
+let quiz_i_3__in = document.querySelector(".quiz_i_3 select");
 let quiz_i_4__in = document.querySelector(".quiz_i_4 input");
 let quiz_i_5__in = document.querySelector(".quiz_i_5 input");
 
 let q_err = document.querySelector(".q_err");
 let q__countttt = document.querySelector(".q__countttt span");
 
+let howsBtn = false;
 function show_btn() {
-  quiz_i_1__in.value,
-    quiz_i_2__in.value,
-    quiz_i_3__in.value,
-    quiz_i_4__in.value != ""
-      ? q_subm.classList.remove("q_subm-b")
-      : q_subm.classList.add("q_subm-b");
+  //   quiz_i_1__in.value,
+  //     quiz_i_2__in.value,
+  //     quiz_i_3__in.value,
+  //     quiz_i_4__in.value != ""
+  //       ? q_subm.classList.remove("q_subm-b")
+  //       : q_subm.classList.add("q_subm-b");
+
+  if (
+    (quiz_i_1__in.value != "Выберите услугу",
+    quiz_i_3__in.value != "Выберите объект",
+    quiz_i_2__in.value != "",
+    quiz_i_5__in.value != "",
+    quiz_i_4__in.value != "")
+  ) {
+    howsBtn = true;
+  }
+  howsBtn
+    ? q_subm.classList.remove("q_subm-b")
+    : q_subm.classList.add("q_subm-b");
 }
 
 function main_qqq() {
@@ -155,6 +174,18 @@ function main_qqq() {
 function check_in(input, mess, num) {
   //debugger
   if (input.value === "") {
+    q_err.innerHTML =
+      q_err.innerHTML +
+      `<div onclick="q_count = ${+num} , main_qqq()" > <span>${
+        num + 1
+      }</span> ${mess}</div>`;
+  } else if (input.value === "Выберите услугу") {
+    q_err.innerHTML =
+      q_err.innerHTML +
+      `<div onclick="q_count = ${+num} , main_qqq()" > <span>${
+        num + 1
+      }</span> ${mess}</div>`;
+  } else if (input.value === "Выберите объект") {
     q_err.innerHTML =
       q_err.innerHTML +
       `<div onclick="q_count = ${+num} , main_qqq()" > <span>${
@@ -218,28 +249,20 @@ document.addEventListener("click", function () {
     q_next.addEventListener("click", function () {
       q_err.innerHTML = "";
       if (q_count === 3) {
-        check_in(quiz_i_1__in, "Укажите ваше имя", 0);
-        check_in(quiz_i_2__in, "Выберите из списка", 1);
-        check_in(quiz_i_3__in, "Введите площадь м2", 2);
-        check_in(
-          quiz_i_4__in,
-          "Введите электронную почту в правильном формате",
-          3
-        );
+        check_in(quiz_i_1__in, "Выберите объект из списка", 0);
+        check_in(quiz_i_2__in, "Введите площадь м2", 1);
+        check_in(quiz_i_3__in, "Выберите услугу из списка", 2);
+        check_in(quiz_i_4__in, "Укажите ваше имя", 3);
         // check_in(
         //   quiz_i_5__in,
         //   "5 Введите Номер телефонa в правильном формате",
         //   4
         // );
       } else if (q_count === 4) {
-        check_in(quiz_i_1__in, "Укажите ваше имя", 0);
-        check_in(quiz_i_2__in, "Выберите из списка", 1);
-        check_in(quiz_i_3__in, "Введите площадь м2", 2);
-        check_in(
-          quiz_i_4__in,
-          "Введите электронную почту в правильном формате",
-          3
-        );
+        check_in(quiz_i_1__in, "Выберите объект из списка", 0);
+        check_in(quiz_i_2__in, "Введите площадь м2", 1);
+        check_in(quiz_i_3__in, "Выберите услугу из списка", 2);
+        check_in(quiz_i_4__in, "Укажите ваше имя", 3);
         check_in(
           quiz_i_5__in,
           "Введите Номер телефонa в правильном формате",
